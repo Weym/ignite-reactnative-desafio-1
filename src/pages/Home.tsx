a83f324deedf84c2d886b00dc5160a89db31e41c
Weydson Marinho <weydsonmarinho@gmail.com>
2022-10-28 12:21:36 -0300

    Second Challenge Completed
    Edit a task
    Show Alerts for confirmation and feedback

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,28 @@
 import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { Alert, StyleSheet, View } from "react-native";
 
 import { Header } from "../components/Header";
 import { Task, TasksList } from "../components/TasksList";
 import { TodoInput } from "../components/TodoInput";
 
+export type EditTaskProps = {
+  taskId: number;
+  taskNewTitle: string;
+};
+
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
+    const taskExists = tasks.find((task) => task.title === newTaskTitle);
+
+    if (taskExists) {
+      return Alert.alert(
+        "Task já cadastrada",
+        "Você não pode cadastrar uma task com o mesmo nome"
+      );
+    }
+
     const newTask = {
       id: new Date().getTime(),
       title: newTaskTitle,
@@ -25,11 +39,38 @@ export function Home() {
     });
 
     setTasks(updatedTasks);
-    console.log(tasks);
   }
 
   function handleRemoveTask(id: number) {
-    const updatedTasks = tasks.filter((task) => id !== task.id);
+    Alert.alert(
+      "Remover item",
+      "Tem certeza que você deseja remover esse item?",
+      [
+        {
+          style: "cancel",
+          text: "Não",
+        },
+        {
+          style: "destructive",
+          text: "Sim",
+          onPress: () => {
+            const updatedTasks = tasks.filter((task) => id !== task.id);
+
+            setTasks(updatedTasks);
+          },
+        },
+      ]
+    );
+  }
+
+  function handleEditTask({ taskId, taskNewTitle }: EditTaskProps) {
+    const updatedTasks = [...tasks];
+
+    const tasksToBeUpdated = updatedTasks.find((task) => task.id === taskId);
+
+    if (!tasksToBeUpdated) return;
+
+    tasksToBeUpdated.title = taskNewTitle;
 
     setTasks(updatedTasks);
   }
@@ -44,6 +85,7 @@ export function Home() {
         tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
         removeTask={handleRemoveTask}
+        editTask={handleEditTask}
       />
     </View>
   );
